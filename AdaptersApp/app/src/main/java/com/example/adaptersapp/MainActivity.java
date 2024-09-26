41ea3b56f74285a03d5121f45daae120cefffeb6
Bohdan <163863392+shendiu@users.noreply.github.com>
2024-09-26 13:48:35 +0300

    AdaptersApp

---
+++ AdaptersApp/app/src/main/java/com/example/adaptersapp/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.adaptersapp;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        listView = findViewById(R.id.listview);
+
+        String[] countries = {"Ukraine","USA","Great Britain"};
+
+//        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+//                this,
+//                android.R.layout.simple_list_item_1,
+//                countries
+//        );
+
+        MyCustomAdapter adapter = new MyCustomAdapter(this,countries);
+
+        listView.setAdapter(adapter);
+    }
+}
